b55e9a1e5809ffcda29f0bf0121328bbe249e528
klavs <klavs202@gmail.com>
2019-06-03 11:26:30 +0300

    beigas

--- js2.js
+++ js2.js
@@ -77,6 +77,9 @@ function clearlocal() {
   location.reload();
 }
 
+
+
+
 //Salabot localStorage tā, lai pēc lapas refresha nepazūd pieejamais budžets un kopējā izdevumu un ienākumu vērtība
 //Pievienot iespēju izveidot jaunu ierakstu budzeta kalkulātora izmantojot enter taustinu
 //Izveidot vienu mainīgo, kurā veido sarakstu ar ienākumiem un izdevumiem un to visu glabāt objektā, kuru pēc tam saglabā localStorage
@@ -84,5 +87,4 @@ function clearlocal() {
 //Pievienot iespēju nodzēst sarakstu elementu un tas automātiski atrēķinās nost
 //ja lauki ir aizpildīti, tad pievienot ierakstu sarakstam var uzspiežot enter (Poga paliek pieejama)
 //Pievienot iespēju izveidot jaunu mēnesi. Opcija Jauns mēnesis saglabās iepriekšējā mēneša datus un nodzēsīs laukus priekš jaunā mēneša.
-//Pēc vajadzības var atvērt iepriekšējos mēnešus un apskatīt to ienākumus un izdevumus
-
+//Pēc vajadzības var atvērt iepriekšējos mēnešus un apskatīt to ienākumus un izdevumu
